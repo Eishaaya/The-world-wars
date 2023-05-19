6a32d0e6a7e949c3c20656a24d32755e017a21bb
Eishaaya <meishaaya@gmail.com>
2023-05-19 13:46:05 -0700

    Ported to monogame 3.8.1
    I hate this code with a burning passion

---
+++ WarExMachina/button.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace The_world_wars_Game
+{
+    class button : sprite
+    {
+        bool isclicked;
+        bool ishovered;
+        protected int _cost;
+        public int cost
+        {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                _cost = value;
+            }
+        }
+        MouseState ms;
+        public button(Texture2D image, Vector2 position, Color color, int cost)
+            : base(image, position, color)
+        {
+            _cost = cost;
+        }
+        public void Update(GameTime gameTime)
+        {
+            ms = Mouse.GetState();
+            if (_hitbox.Contains(ms.X, ms.Y))
+            {
+                ishovered = true;
+                _color = Color.Red;
+                if (ms.LeftButton == ButtonState.Pressed)
+                {
+                    _color = Color.Maroon;
+                    isclicked = true;
+                }
+                else
+                {
+                    isclicked = false;
+                }
+                //you clicked on this
+            }
+            else
+            {
+                _color = Color.White;
+                ishovered = false;
+            }
+            
+        }
+    }
+}
---
+++ WarExMachina/sprite.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace The_world_wars_Game
+{
+    class sprite
+    {
+        protected Vector2 _location;
+        protected Color _color;
+        protected Texture2D _image;
+        protected Rectangle _hitbox;
+        protected SpriteEffects _effects;
+
+
+
+        public Vector2 Location
+        {
+            get
+            {
+                
+                return _location;
+                
+            }
+            set
+            {
+                _hitbox.X = (int)_location.X;
+                _hitbox.Y = (int)_location.Y;
+                _location = value;
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+        public Rectangle Hitbox
+        {
+            get
+            {
+                return _hitbox;
+            }
+            set
+            {
+                _hitbox = value;
+            }
+        }
+        public Texture2D Image
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                _image = value;
+            }
+        }
+        public sprite(Texture2D image, Vector2 position, Color color)
+        {
+            _image = image;
+            _location = position;
+            _color = color;
+            _hitbox = new Rectangle((int)_location.X, (int)_location.Y, _image.Width, _image.Height);
+        }
+        public virtual void draw(SpriteBatch spbt)
+        {
+            spbt.Draw(_image, _location, null, _color, 0f, Vector2.Zero, 1f, _effects, 1f);
+        }
+        public void debugDraw(SpriteBatch batch, Texture2D pixel)
+        {
+            batch.Draw(pixel, _hitbox, Color.Red);
+        }
+    }
+}
